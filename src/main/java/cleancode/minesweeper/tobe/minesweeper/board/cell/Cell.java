8a1b8335ef65246eb7b0f55804cf0a622569ff2e
seotaemin <taeminseo0115@gmail.com>
2024-10-07 21:00:17 +0900

    [24.10.05] section 6-3. 패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/Cell.java
@@ -0,0 +1,22 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public interface Cell {
+
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
+
+    boolean isLandMine();
+
+    boolean hasLandMineCount();
+
+    CellSnapshot getSnapshot();
+
+    void flag();
+
+    void open();
+
+    boolean isChecked();
+
+    boolean isOpened();
+
+}
